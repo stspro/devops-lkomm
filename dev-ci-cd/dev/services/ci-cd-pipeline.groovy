471eff81bbddeb956d19258cd41fa8b3303f5d93
stspro <42194755+stspro@users.noreply.github.com>
2022-01-11 23:52:19 -0500

    Create ci-cd-pipeline.groovy

---
+++ dev-ci-cd/dev/services/ci-cd-pipeline.groovy
@@ -0,0 +1,27 @@
+node{
+  stage(checkout)
+   
+   git clone https://github.com/stspro/spring-boot.git
+     //dir (")
+     bat "dir *.*/s"
+   bat "cd web-thymeleaf-war; mvn package"
+  
+   //copy web-thymeleaf-war/target/mkyong.war $TOMCAT/webapps/
+   //start Tomcat
+   //localhost:8080/mkyong
+  
+  stage(build automation){
+    mvn package
+  }
+  
+  stage(unit testing){
+  }
+  
+  stage(code analysis){
+  }
+  
+  stage(build management){
+  //artifactory upload
+  }
+  
+  
