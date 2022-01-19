ea95693fe6a6958eab235150a5397f87a58ba420
stspro <42194755+stspro@users.noreply.github.com>
2022-01-18 19:35:33 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws"{
+    region = "us-east-1"
+    access_key = "AKIAUA4YV4XD626KRZN5"
+    secret_key = "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"
+}
+
+resource "aws_instance" "my_first_server"{
+   ami = "ami-0ed9277fb7eb570c9"
+   instance_type = "t2.micro"
+
+
+user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  echo "*** Completed Installing apache2"
+  EOF
+
+   tags = {
+    Name = "Web_Instance"
+  }
+
+}
+/*
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  */
