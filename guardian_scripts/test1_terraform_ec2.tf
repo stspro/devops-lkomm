db9205aca17dd4cfb16bd20e7e22a8d1373d3fda
stspro <42194755+stspro@users.noreply.github.com>
2024-05-22 14:26:15 -0400

    Update test1_terraform_ec2.tf

--- guardian_scripts/test1_terraform_ec2.tf
+++ guardian_scripts/test1_terraform_ec2.tf
@@ -1,28 +1,56 @@
-# Terraform HCL
-
 provider "aws" {
-  region = "us-east-1"
+  region     = "US-east-1"
+  }
+
+resource "aws_instance" "web_server" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    key_name = "ec2_keypair"
+	
+    
+    tags = {
+        Name = "web-server-instance"
+    }
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-07d0cf3af28718ef8"
-  instance_type = "t2.micro"
-  
-  
-   user_data = <<-EOF
-	#!/bin/bash   
-	sudo apt update -y
-	sudo apt-cache search tomcat7.0 -y
-	sudo apt install tomcat7.0 tomcat-webapps tomcat-admin-webapps -y
-	cd /usr/share/tomcat/webapps/
-	ss -ltn
-	sudo wget <https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample.war>
-	sudo systemctl enable tomcat7.0
-	
-	EOF
-  
+resource "aws_security_group" "web_server_sg" {
+ name = "web_server_sg"
+ description = "security group of the web server"
 
-  tags = {
-    Name = "HelloTerraform"
+
+  
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
+  }
+
+
+#Provisioner to install and configure Apache Web Server
+ user_data = <<-EOF
+      #!/bin/bash
+      yum update -y
+      yum install -y httpd
+      sudo systemctl start httpd
+      sudo systemctl enable httpd
+      echo "<html><body><h1>Hello,World!<h1><body><html>" > /var/www/html/index.html
+      EOF
+	
+    
+}
+
+
+# outputs
+output"web_server_public_ip"{
+  value =  aws_instance.web_server.public_ip
 }
